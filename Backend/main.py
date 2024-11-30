e5019fc4b26948b14eba56558bbc070f52414f88
QoutiOussama13 <qoutioussama2002@gmail.com>
2024-11-30 23:09:13 +0100

    Add CORS support and serve static files in FastAPI application

--- Backend/main.py
+++ Backend/main.py
@@ -1,19 +1,40 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from agents.agent_chek_relevence import *
 from utils.check_model_status import *
 from agents.skills_assessment_agent import *
 from agents.skills_assessement_evaluator_agent import *
 from entities.skills_entities import *
 from entities.learning_path_entities import *
+from fastapi.responses import HTMLResponse
+
 from agents.learning_path_agent import *
 app = FastAPI()
+
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust this to the specific origins you want to allow
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Serve static files
+
 model = get_model("gpt-4o-mini")
-@app.get("/")
-def read_root():
-    return {"message": model_name}
+app.mount("/assets", StaticFiles(directory="../Frontend/assets"), name="assets")
+app.mount("/media", StaticFiles(directory="../Frontend/assets/media"), name="media")
 
 
-@app.post("/chek-relevnce")
+@app.get("/", response_class=HTMLResponse)
+async def read_index():
+    with open("../Frontend/index.html", "r") as file:
+        return HTMLResponse(content=file.read(), status_code=200)
+
+@app.post("/chek-relevence")
 def check_relevence(query: str):
     result = check_relevence_agent(model,query)
     return {"message": result}
